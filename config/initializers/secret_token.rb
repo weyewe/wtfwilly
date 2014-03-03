235c1dce336800a19992530ac46341c6b09fdaaf
weyewe <w.yunnal@gmail.com>
2014-03-03 07:14:14 +0700

    wtfwilly initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wtfwilly::Application.config.secret_key_base = '1cee6661eab2fb2c6f8a093ee6ad5267536288c23426965155b35eaf2f930b023b123661d6d0813d5714b813e0ca4b088add27ed466e0476f94d289ee06c27b8'
